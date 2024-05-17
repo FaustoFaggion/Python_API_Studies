bb99f9228afcc60f3b140766761dff5781356fd8
FaustoFaggion <faustofaggion@hotmail.com>
2024-05-17 12:01:54 -0300

    Create a validation dto with jsonschema library

--- backend/src/users/user_service.py
+++ backend/src/users/user_service.py
@@ -2,31 +2,52 @@ from flask import request, jsonify
 from src.users.user_repository import UserRepository
 from src.users.dto.input_dto import CreateUserDto
 
+import jsonschema
+from jsonschema import validate
+
+CREATE_USER_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "email": {"type": "string"},
+        "name": {"type": "string"},
+        "age": {"type": "integer"},
+        "password": {"type": "string"}
+    },
+    "required": ["email", "name", "age", "password"]
+}
+
 class UserService():
     
     def __init__(self):
         pass
 
-    def validate_dto():
-        json_data = request.get_json()
+    def validate_dto(json_data: str):
         try:
             if not json_data:
                 return jsonify({ "error": "Invalid JSON data"})    
+
+            validate(instance=json_data, schema=CREATE_USER_SCHEMA)    
             dto: CreateUserDto = CreateUserDto(json_data)
-        except KeyError as err:
-            return jsonify({"error": str(err)}), 400
+        
+        except jsonschema.ValidationError as e:
+            return jsonify({"error": str(e)}), 400
         
         return dto
         
     def create():
         json_data = request.get_json()
+        
         try:
             if not json_data:
                 return jsonify({ "error": "Invalid JSON data"})    
+
+            validate(instance=json_data, schema=CREATE_USER_SCHEMA)    
             dto: CreateUserDto = CreateUserDto(json_data)
-        except KeyError as err:
-            return jsonify({"error": str(err)}), 400
         
+        except jsonschema.ValidationError as e:
+            return jsonify({"error": str(e)}), 400
+        
+        # dto = UserService.validate_dto(json_data)        
         user = UserRepository.create(dto)
         
         return user
